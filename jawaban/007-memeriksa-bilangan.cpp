b6489e370b54bb25a232dc3eeea8944eff33dea3
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-05-06 08:53:12 +0700

    update jawaban/007-memeriksa-bilangan.cpp

---
+++ jawaban/007-memeriksa-bilangan.cpp
@@ -0,0 +1,9 @@
+
+
+
+
+/*
+
+Tuliskan dan buatkan program C++ untuk memeriksa 3 bilangan yang diinput. output harus memunculkan bilangan yang paling besar dan bilangan kecil, lalu apakah bilangan itu positif atau negatif!
+
+*/
