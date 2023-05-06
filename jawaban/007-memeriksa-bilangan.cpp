ad542fce691323402ba43c86c6b67eb05e28154c
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-05-06 08:55:37 +0700

    update jawaban/007-memeriksa-bilangan.cpp

--- jawaban/007-memeriksa-bilangan.cpp
+++ jawaban/007-memeriksa-bilangan.cpp
@@ -50,7 +50,7 @@ int main() {
     } else {
         cout << c << " adalah nol" << endl;
     }
-        /*
+/*
 Tuliskan dan buatkan program C++ untuk memeriksa 3 bilangan yang diinput. output harus memunculkan bilangan yang paling besar dan bilangan kecil, lalu apakah bilangan itu positif atau negatif!
 */
     cout << "Bilangan terbesar adalah " << max << endl;
