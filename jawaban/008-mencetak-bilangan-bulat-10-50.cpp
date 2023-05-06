f00f0406fdd744ecdadff4550296df9960e6c749
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-05-06 08:59:55 +0700

    update jawaban/008-mencetak-bilangan-bulat-10-50.cpp

--- jawaban/008-mencetak-bilangan-bulat-10-50.cpp
+++ jawaban/008-mencetak-bilangan-bulat-10-50.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int namamu = 0; // inisialisasi hitungan bilangan
-    for (int i = 10; i <= 50; i++) {
-        cout << i << " "; // cetak bilangan
-        tia++; // tambahkan hitungan
+  int namamu = 0; // inisialisasi hitungan bilangan
+  for (int i = 10; i <= 50; i++) {
+       cout << i << " "; // cetak bilangan
+       tia++; // tambahkan hitungan
  }
  cout << endl;
  cout << "Jumlah bilangan: " << tia << endl;
