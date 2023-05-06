ebe7ab5ce397f684e7cfdf08aeada219285ab295
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-05-06 08:50:38 +0700

    update jawaban/001-ganjil-genap.cpp

---
+++ jawaban/001-ganjil-genap.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int andhika;
+   cout << "===[ X TKJ 1 ]===\n";
+   cout << "Masukkan bilangan: ";
+   cin >> andhika;
+        // X TKJ 1 SMK RADEN PAKU
+   if (namakalian % 2 == 0) {
+      cout << andhika << " adalah bilangan genap." << endl;
+   } else {
+      cout << andhika << " adalah bilangan ganjil." << endl;
+   }
+
+   return 0;
+}
